4b28e77675cc127621ac7d88227d73e5d0f360d4
statusup <amostra.gratis.seguidores@gmail.com>
2025-04-28 12:15:05 -0300

    Create color-kit.user.js

---
+++ components/color/color-kit.user.js
@@ -0,0 +1,29 @@
+.tamperkit-color-success {
+    background-color: #4CAF50 !important;
+    color: #ffffff !important;
+}
+
+.tamperkit-color-done {
+    background-color: #388E3C !important;
+    color: #ffffff !important;
+}
+
+.tamperkit-color-warning {
+    background-color: #FFC107 !important;
+    color: #000000 !important;
+}
+
+.tamperkit-color-danger {
+    background-color: #F44336 !important;
+    color: #ffffff !important;
+}
+
+.tamperkit-color-info {
+    background-color: #2196F3 !important;
+    color: #ffffff !important;
+}
+
+.tamperkit-color-default {
+    background-color: #9E9E9E !important;
+    color: #ffffff !important;
+}
